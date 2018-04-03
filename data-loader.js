277ef3d57c98a77adfe5ff473f85e7584d43f17b
Alexandre Duchenne <alexduch@hotmail.fr>
2018-04-03 21:03:38 +0200

    Refactoring the xp component for consistency

--- data-loader.js
+++ data-loader.js
@@ -11,38 +11,35 @@ Vue.component('skill', {
 Vue.component('xp', {
   props: ['xp', 'skillStyle'],
   template:
-     `<div>
-       <div v-for="x in xp" class="row p-1">
-         <div class="col-12">
-           <div class="card">
-             <div class="card-header">{{ x.dates }}</div>
-             <div class="card-body">
-               <h4 class="card-title">{{ x.position }}</h4>
-               <h6 class="card-subtitle mb-2 text-muted">{{ x.company }}, {{ x.location }}</h6>
-               <p>{{ x.context }}</p>
-               <ul>
-                 <li v-for="task in x.tasks">{{ task }}</li>
-               </ul>
-               <skill v-for="(category, name) in x.env" :name="name" :category="category" :skill-style="skillStyle"></skill>
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>`
+      `<div class="col-12 p-1">
+        <div class="card">
+          <div class="card-header">{{ xp.dates }}</div>
+          <div class="card-body">
+            <h4 class="card-title">{{ xp.position }}</h4>
+            <h6 class="card-subtitle mb-2 text-muted">{{ xp.company }}, {{ xp.location }}</h6>
+            <p>{{ xp.context }}</p>
+            <ul>
+              <li v-for="task in xp.tasks">{{ task }}</li>
+            </ul>
+            <skill v-for="(category, name) in xp.env" :name="name" :category="category" :skill-style="skillStyle"></skill>
+          </div>
+        </div>
+      </div>`
 });
 
 Vue.component('training', {
   props: ['dates', 'school', 'training', 'description'],
-  template: `<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 p-1">
-              <div class="card">
-                <div class="card-header">{{ dates }}</div>
-                <div class="card-body">
-                  <h4 class="card-title">{{ training }}</h4>
-                  <h6 class="card-subtitle mb-2 text-muted">{{ school }}</h6>
-                  <p>{{ description }}</p>
-                </div>
-              </div>
-            </div>`
+  template:
+      `<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 p-1">
+        <div class="card">
+          <div class="card-header">{{ dates }}</div>
+          <div class="card-body">
+            <h4 class="card-title">{{ training }}</h4>
+            <h6 class="card-subtitle mb-2 text-muted">{{ school }}</h6>
+            <p>{{ description }}</p>
+          </div>
+        </div>
+      </div>`
 });
 
 let rootComponent;
